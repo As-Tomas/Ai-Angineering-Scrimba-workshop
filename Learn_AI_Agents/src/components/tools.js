a3a2159db26870d490ef7ca219923903446bb33d
Tomas Bance <tomas.bance@gmail.com>
2024-02-05 20:08:26 +0100

    OpenAI Functions Agent - Part 7 and 8 - Pushing to messages and arguments and locatio by ip

--- Learn_AI_Agents/src/components/tools.js
+++ Learn_AI_Agents/src/components/tools.js
@@ -1,12 +1,52 @@
-export async function getCurrentWeather() {
+export async function getCurrentWeather({ location, unit = "fahrenheit" }) {
+    console.log("location:", location)
+    console.log("unit:", unit)
     const weather = {
-        temperature: "72",
-        unit: "F",
+        location,
+        temperature: "75",
+        unit,
         forecast: "sunny"
     }
     return JSON.stringify(weather)
 }
 
 export async function getLocation() {
-    return "New York City, NY"
+    try {
+        const response = await fetch('https://ipapi.co/json/')
+        const text = await response.json()
+        return JSON.stringify(text)
+      } catch (err) {
+        console.log(err)
+      }
 }
+
+export const tools =[
+    {
+        type: "function",
+        function: {
+            name: "getCurrentWeather",
+            description: "Get the current weather",
+            parameters: {
+                type: "object",
+                properties: {
+                    location: {
+                        type: "string",
+                        description: "The location from where to get the weather"
+                    }
+                },
+                required: ["location"]
+            }
+        }
+    },
+    {
+        type: "function",
+        function: {
+            name: "getLocation",
+            description: "Get the user's current location",
+            parameters: {
+                type: "object",
+                properties: {}
+            }
+        }
+    },
+]
