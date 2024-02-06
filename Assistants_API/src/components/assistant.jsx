00ae8aef79d5c475e585817ec32a6e27926b46e6
Tomas Bance <tomas.bance@gmail.com>
2024-02-06 18:44:06 +0100

    Updated upload file, create assistan, run thread

--- Assistants_API/src/components/assistant.jsx
+++ Assistants_API/src/components/assistant.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import OpenAI from "openai";
-// import { getCurrentWeather, getLocation, tools, functions } from "./tools";
+// https://platform.openai.com/docs/api-reference/threads/createThread;
 
 const OPENAI_API_KEY = import.meta.env.VITE_REACT_APP_OPENAI_API_KEY;
 
@@ -9,51 +9,62 @@ const openai = new OpenAI({
   dangerouslyAllowBrowser: true,
 });
 
-const assistanID= "asst_cM0naaJFJZJXyFNyI5IQ3aSV";
-const file_ids= ["file-zSs8MPpBI9l8JUXAKpgWL1u0"];
-//asst_KPXhtmnL1R63LN1tIyHDixZt
+const MovieExpert = "";
+const file_ids = [""];
 
+const threadID = "";
 
+export default function Assistant() {
+  const [query, setQuery] = useState("");
+  const [response, setResponse] = useState("");
 
+  const handleQueryChange = (event) => {
+    setQuery(event.target.value);
+  };
 
-  export default function Assistant() {
-    const [query, setQuery] = useState("");
-    const [response, setResponse] = useState("");
-
-    const handleQueryChange = (event) => {
-      setQuery(event.target.value);
-    };
-
-    const handleSubmit = async (event) => {
-      event.preventDefault();
-      const agentResponse = await createAssistant(query);
-      setResponse(agentResponse);
-    };
+  const handleSubmit = async (event) => {
+    event.preventDefault();
     // 1) run code upload file and save its Id
-    // 2) create assistant wit file ID and save assistans id 
+    // 2) create assistant wit file ID and save assistans id
+    // 3) create thread and save thread id
+    //  const agentResponse = await uploadFile();
+    // const agentResponse = await createAssistant(query);
+    // const agentResponse = await CreateThread();
+    setResponse(agentResponse);
+  };
 
-    // Upload a file with an "assistants" purpose
-    // const file = await openai.files.create({
-    //   file: await fetch("movies.txt"),
-    //   purpose: "assistants",
-    // });
-    // console.log(file)
+  // Upload a file with an "assistants" purpose
+  async function uploadFile() {
+    const file = await openai.files.create({
+      file: await fetch("movies.txt"),
+      purpose: "assistants",
+    });
+    console.log(file);
+    return file.id;
+  }
 
-    // Create Movie Expert Assistant
-    async function createAssistant(query) {
-      const myAssistant = await openai.beta.assistants.create({
-        instructions: `You are great at recommending movies. When asked a question, use the information 
+  // Create Movie Expert Assistant
+  async function createAssistant(query) {
+    const myAssistant = await openai.beta.assistants.create({
+      instructions: `You are great at recommending movies. When asked a question, use the information 
       in the provided file to form a friendly response. If you cannot find the answer in the file, do 
       your best to infer what the answer should be.`,
-        name: "Movie Expert2",
-        tools: [{ type: "retrieval" }],
-        model: "gpt-4-1106-preview",
-        file_ids: file_ids,
-      });
+      name: "Movie Expert2",
+      tools: [{ type: "retrieval" }],
+      model: "gpt-4-1106-preview",
+      file_ids: file_ids,
+    });
 
-      console.log(myAssistant);
-      return myAssistant;
-    } 
+    console.log(myAssistant);
+    return myAssistant.id;
+  }
+
+  // Create thread
+  async function CreateThread() {
+    const thread = await openai.beta.threads.create();
+    console.log(thread);
+    return thread.id;
+  }
 
   return (
     <div>
