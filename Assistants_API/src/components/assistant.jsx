a80ef34d3076b6a17b81b702295d40e1aa36c9ed
Tomas Bance <tomas.bance@gmail.com>
2024-02-07 17:14:12 +0100

    Bringing all together

--- Assistants_API/src/components/assistant.jsx
+++ Assistants_API/src/components/assistant.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import OpenAI from "openai";
 // https://platform.openai.com/docs/api-reference/threads/createThread;
 
@@ -10,9 +10,10 @@ const openai = new OpenAI({
 });
 
 const MovieExpert = "asst_HIPK2NVtxjkt9PYEmzQmtgoq";
+const asstID = "asst_HIPK2NVtxjkt9PYEmzQmtgoq";
 const file_ids = ["file-UuVlCAXZDOHHl8fgrYZEtkkM"];
 
-let threadID = "thread_kGsRo2OJUrtF5ID89G8t8Eh8";
+let threadID = null;
 let runID = "run_4YW1W73dp3W6pwTDGcWAC5uQ";
 
 export default function Assistant() {
@@ -21,51 +22,8 @@ export default function Assistant() {
 
   const handleQueryChange = (event) => {
     setQuery(event.target.value);
-  };
+  };  
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    // 1) run code upload file and save its Id
-    // 2) create assistant wit file ID and save assistans id
-    // 3) create thread and save thread id
-    // const agentResponse = await uploadFile();
-    // const agentResponse = await createAssistant(query);
-
-    // const agentResponse = await CreateThread();
-    // const agentResponse = await createMessage();
-    // const agentResponse = await runThread();
-    // const agentResponse = await getRun();
-    const agentResponse = await listMessages();
-    setResponse(agentResponse);
-  };
-
-  // Upload a file with an "assistants" purpose
-  async function uploadFile() {
-    const file = await openai.files.create({
-      file: await fetch("movies.txt"),
-      purpose: "assistants",
-    });
-    console.log(file);
-    return file.id;
-  }
-
-  // Create Movie Expert Assistant
-  async function createAssistant(query) {
-    const myAssistant = await openai.beta.assistants.create({
-      instructions: `You are great at recommending movies. When asked a question, use the information 
-      in the provided file to form a friendly response. If you cannot find the answer in the file, do 
-      your best to infer what the answer should be.`,
-      name: "Movie Expert3",
-      tools: [{ type: "retrieval" }],
-      model: "gpt-4-1106-preview",
-      file_ids: file_ids,
-    });
-
-    console.log(myAssistant);
-    return myAssistant.id;
-  }
-
-  // Create thread
   async function CreateThread() {
     const thread = await openai.beta.threads.create();
     console.log(thread);
@@ -73,47 +31,149 @@ export default function Assistant() {
     return thread.id;
   }
 
-  async function createMessage() {
+  async function createMessage(query) {
     const threadMessages = await openai.beta.threads.messages.create(threadID, {
       role: "user",
-      content: "Can you recommend a comedy?",
+      content: query,
     });
     console.log(threadMessages);
   }
 
-  // Run the assistant's thread
+  // Run the thread / assistant
   async function runThread() {
     const run = await openai.beta.threads.runs.create(threadID, {
-      assistant_id: MovieExpert,
+      assistant_id: asstID,
+      instructions: `Please do not provide annotations in your reply. Only reply about movies in the provided file.
+       If questions are not related to movies, respond with "Sorry, I don't know." Keep your answers short.`,
     });
-    console.log(run);
-    runID = run.id;
-    return run.id;
+    return run;
+  }
+
+  // List thread Messages
+  async function listMessages() {
+    return await openai.beta.threads.messages.list(threadID);
   }
 
   // Get the current run
-  async function getRun() {
-    const currentRun = await openai.beta.threads.runs.retrieve(
-      threadID,
-      runID
-    );
-    console.log("Run status: " + currentRun.status);
-    return currentRun.status;
+  async function retrieveRun(thread, run) {
+    return await openai.beta.threads.runs.retrieve(thread, run);
   }
 
-  // List thread messages
-  async function listMessages() {
-    const threadMessages = await openai.beta.threads.messages.list(threadID);
+  const handleSubmit = async (event) => {
+    event.preventDefault();
 
-    console.log(threadMessages.data);
-    return threadMessages.data[0].content[0].text.value;
-}
-  
-  
+    if (!threadID) {
+      const newThreadId = await CreateThread();
+      threadID = newThreadId;
+    }
+
+    setResponse("Thinking...");
+    await createMessage(query);
+    // Create a run
+    const run = await runThread();
+    // Retrieve the current run
+    let currentRun = await retrieveRun(threadID, run.id);
+
+    // Keep Run status up to date
+    // Poll for updates and check if run status is completed
+    while (currentRun.status !== "completed") {
+      await new Promise((resolve) => setTimeout(resolve, 1500));
+      console.log(currentRun.status);
+      currentRun = await retrieveRun(threadID, run.id);
+    }
+
+    // Get messages from the thread
+    const { data } = await listMessages();
+    setResponse(data[0].content[0].text.value);
+
+    
+  };
+
+  //----------------------------------------------
+  // 1) run code upload file and save its Id
+  // 2) create assistant wit file ID and save assistans id
+  // 3) create thread and save thread id
+  // const agentResponse = await uploadFile();
+  // const agentResponse = await createAssistant(query);
+
+  // const agentResponse = await CreateThread();
+  // const agentResponse = await createMessage();
+  // const agentResponse = await runThread();
+  // const agentResponse = await getRun();
+  // const agentResponse = await listMessages();
+  //   // Upload a file with an "assistants" purpose
+  //   async function uploadFile() {
+  //     const file = await openai.files.create({
+  //       file: await fetch("movies.txt"),
+  //       purpose: "assistants",
+  //     });
+  //     console.log(file);
+  //     return file.id;
+  //   }
+
+  //   // Create Movie Expert Assistant
+  //   async function createAssistant(query) {
+  //     const myAssistant = await openai.beta.assistants.create({
+  //       instructions: `You are great at recommending movies. When asked a question, use the information
+  //       in the provided file to form a friendly response. If you cannot find the answer in the file, do
+  //       your best to infer what the answer should be.`,
+  //       name: "Movie Expert3",
+  //       tools: [{ type: "retrieval" }],
+  //       model: "gpt-4-1106-preview",
+  //       file_ids: file_ids,
+  //     });
+
+  //     console.log(myAssistant);
+  //     return myAssistant.id;
+  //   }
+
+  //   // Create thread
+  //   async function CreateThread() {
+  //     const thread = await openai.beta.threads.create();
+  //     console.log(thread);
+  //     threadID = thread.id;
+  //     return thread.id;
+  //   }
+
+  //   async function createMessage() {
+  //     const threadMessages = await openai.beta.threads.messages.create(threadID, {
+  //       role: "user",
+  //       content: "Can you recommend a comedy?",
+  //     });
+  //     console.log(threadMessages);
+  //   }
+
+  //   // Run the assistant's thread
+  //   async function runThread() {
+  //     const run = await openai.beta.threads.runs.create(threadID, {
+  //       assistant_id: MovieExpert,
+  //     });
+  //     console.log(run);
+  //     runID = run.id;
+  //     return run.id;
+  //   }
+
+  //   // Get the current run
+  //   async function getRun() {
+  //     const currentRun = await openai.beta.threads.runs.retrieve(
+  //       threadID,
+  //       runID
+  //     );
+  //     console.log("Run status: " + currentRun.status);
+  //     return currentRun.status;
+  //   }
+
+  //   // List thread messages
+  //   async function listMessages() {
+  //     const threadMessages = await openai.beta.threads.messages.list(threadID);
+
+  //     console.log(threadMessages.data);
+  //     return threadMessages.data[0].content[0].text.value;
+  // }
 
   return (
     <div>
-      <p>Ai agent new way bysaing vector databases</p>
+      <p>MovieExpert Ai agent new way bypasing vector databases</p>
 
       <form onSubmit={handleSubmit}>
         <input
