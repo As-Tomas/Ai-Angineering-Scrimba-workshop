5c0b20e742869e6bd40e0a51a1a48bcf895add5c
Tomas Bance <tomas.bance@gmail.com>
2024-05-04 12:44:35 +0200

    init vite

---
+++ Dall-e_and_GPT-vision/Vite/index.js
@@ -0,0 +1,40 @@
+import OpenAI from "openai";
+const openai = new OpenAI({
+  apiKey: import.meta.env.VITE_OPENAI_API_KEY,
+  dangerouslyAllowBrowser: true,
+});
+
+async function generateImage() {
+  let prompt = document.getElementById("prompt").value;
+  const loading = document.getElementById("loading");
+  const results = document.getElementById("results");
+
+  if (prompt === "") {
+    prompt = "A colorful coral reef with diverse marine life.";
+  }
+
+  loading.style.display = "block";
+
+  let image;
+  try {
+    image = await openai.images.generate({
+      model: "dall-e-3",
+      prompt: prompt,
+    });
+  } catch (error) {
+    console.log("error", error);
+  }
+  console.log("image", image);
+
+  loading.style.display = "none";
+
+  const row = document.createElement("div");
+  row.innerHTML = `
+  <p>Prompt: ${prompt}</p>
+  <p>Revised Prompt: ${image.data[0].revised_prompt}</p>
+  <img src="${image.data[0].url}" alt="Generated Image">
+  `;
+  results.appendChild(row);
+}
+
+document.getElementById("generate").addEventListener("click", generateImage);
